6fa61d36f2564b2d4b3f8aa258b17c333d3dfba0
NilsVA <NilsVA@example.com>
2015-12-07 17:42:07 +0100

    Fixed, at least i hope so.

--- euler_osc.cxx
+++ euler_osc.cxx
@@ -14,7 +14,7 @@ int main()
 	double x0 = 1.0;
 	double dx0 = 0.0;
 	double tmax = 20*M_PI;
-	double dt = M_PI/10;
+	double dt = M_PI/200;
 	int n = tmax/dt;
 	double x_ana;	
 	double dx_ana;
@@ -23,8 +23,8 @@ int main()
 	double f2[2];
 	
 
-	f1[1]=x0;f1[2]=dx0;
-	f2[1]=x0;f2[2]=dx0;
+	f1[0]=x0;f1[1]=dx0;
+	f2[0]=x0;f2[1]=dx0;
 	
 
 	eulerf(f1,n,dt);
@@ -33,23 +33,23 @@ int main()
 	ofstream outputosc("ana.txt");
 	for(int i=0;i<n;i++)
 	{
-	 x_ana = sin(i*dt);
-	 dx_ana = cos(i*dt);
-	 outputosc << x_ana << "\t" << dx_ana << endl;
+	 x_ana = cos(i*dt);
+	 dx_ana =(-1.0)*sin(i*dt);
+	 outputosc << (i+1)*dt << "\t" << x_ana << "\t" << dx_ana << endl;
 	}
 	outputosc.close();
 }
 
 void eulerf(double* f1, int n, double dt)
 {
-	double a=f1[2];
+	double a=f1[1];
 	ofstream outputf("eulerf.txt");
 	for(int i=1;i<n;i++)
 	{
-	 f1[2]=f1[2]+dt*(-1.0)*f1[1];
-	 f1[1]=f1[1]+dt*a;
-	 a=f1[2];
-	 outputf << f1[1] << "\t" << f1[2] << endl;
+	 f1[1]=f1[1]+dt*(-1.0)*f1[0];
+	 f1[0]=f1[0]+dt*a;
+	 a=f1[1];
+	 outputf << i*dt  << "\t" << f1[0] << "\t" << f1[1] << endl;
 	}
 	outputf.close();
 
@@ -57,15 +57,15 @@ void eulerf(double* f1, int n, double dt)
 
 void eulerb(double* f2, int n, double dt)
 {	
-	double b = f2[1];
+	double b = f2[0];
 	ofstream outputb("eulerb.txt");
 	for(int i=1;i<n;i++)
 	{
 	 
-	 f2[1]=(f2[2]*dt + f2[1])/(1+pow(dt,2));
-	 f2[2]=(f2[2] - dt*b)/(1+pow(dt,2));
-	 b = f2[1];
-	 outputb << f2[1] << "\t" << f2[2] << endl;
+	 f2[0]=(f2[1]*dt + f2[0])/(1+pow(dt,2));
+	 f2[1]=(f2[1] - dt*b)/(1+pow(dt,2));
+	 b = f2[0];
+	 outputb << i*dt << "\t" << f2[0] << "\t" << f2[1] << endl;
 	}
 	outputb.close();
 }
